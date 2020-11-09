c69074cbc98084523140b83e8876f8bcfe38192e
edyaa <ahunovedik@mail.ru>
2020-11-09 15:40:33 +0300

    added_memove

---
+++ ft_memmove.c
@@ -0,0 +1,21 @@
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*dest_tmp;
+	const char	*src_tmp;
+	size_t		i;
+	char		tmp[n];
+
+	i = -1;
+	dest_tmp = dest;
+	src_tmp = src;
+
+	if (!dest_tmp | !src_tmp)
+		return (NULL);
+	
+	while (++i < n)
+		tmp[i] = src_tmp[i];
+
+	i = -1;
+	while (tmp[++i])
+		dest_tmp[i] = tmp[i];
+}
