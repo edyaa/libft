abfc4510123be30cb90d7235cf76df90b5567c1c
edyaa <ahunovedik@mail.ru>
2020-11-09 14:47:12 +0300

    added_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,18 @@
+int		ft_memcmp(const void *arr1, const void *arr2, size_t n)
+{
+	size_t		i;
+	const char	*arr1_tmp;
+	const char	*arr2_tmp;
+
+	i = 0;
+	arr1_tmp = arr1;
+	arr2_tmp = arr2;
+	while (i < n)
+	{
+		if (arr1_tmp[i] == arr2_tmp[i])
+			i++;
+		else
+			return (arr1_tmp[i] - arr2_tmp[i]);
+	}
+	return (0);
+}
