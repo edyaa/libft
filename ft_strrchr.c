d115dc66a525d74f67e78293d447f66088bd747e
edyaa <ahunovedik@mail.ru>
2020-11-09 15:07:06 +0300

    added_strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,18 @@
+char	*ft_strrchr(const void *str, int ch)
+{
+	const char	*src;
+	int			i;
+	
+	src = str;
+	i = 0;
+	if (!src)
+		return (NULL);
+
+	while (src[i])
+	{
+		if (src[i] == ch)
+			return ((char*)src + i);
+		i++;
+	}
+	return (NULL);
+}
