085be4aad2b18193507f51525a22fff2df143184
edyaa <ahunovedik@mail.ru>
2020-11-09 14:25:28 +0300

    added_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,49 +1,15 @@
-#include <stdio.h>
-
-
 void	*ft_memchr(const void *arr, int c, size_t n)
 {
-	const char	arr_tmp;
+	const char	*arr_tmp;
 	size_t		i;
-	int			flag;
 
 	i = 0;
-	flag = 0;
 	arr_tmp = arr;
 	while (i < n)
 	{
-		if (arr_tmp[i] != c)
-		{
-			flag++;
-		}
-		else
-			break;
+		if (*((char*)arr_tmp + i) == c)
+			return ((char*)arr_tmp + i);
 		i++;
 	}
-	if (flag == 0)
-		return (NULL);
-}
-
-int		main(void)
-{
-	// Исходный массив
-   unsigned char src[15]="1234567890";
-   // Переменная, в которую будет сохранен указатель
-   // на искомый символ.
-   char *sym;
-
-   // Вывод исходного массива
-   printf ("src old: %s\n",src);
-
-   // Поиск требуемого символа
-   sym = ft_memchr (src, '4', 10);
-
-   // Если требуемый символ найден, то заменяем его
-   // на символ '!'
-   if (sym != NULL)
-      sym[0]='!';
-
-   // Вывод исходного массива
-   printf ("src new: %s\n",src);
-	return (0);
+	return (NULL);
 }
