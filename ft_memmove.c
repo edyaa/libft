26f81100a21208939644b6c401d0e55d77be5302
Kara Blaze <kblaze@et-n6.kzn.21-school.ru>
2020-11-21 16:04:27 +0300

    fixed_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kblaze <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 16:13:32 by kblaze            #+#    #+#             */
-/*   Updated: 2020/11/21 15:57:55 by kblaze           ###   ########.fr       */
+/*   Updated: 2020/11/21 16:03:32 by kblaze           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	dest_tmp = (char*)dest;
 	src_tmp = (const char*)src;
 	if (dest_tmp >= src_tmp)
-	{
-		printf("dest > src: Нет перекрытия!\n");
 		while (n > ++i)
 			dest_tmp[n - i - 1] = src_tmp[n - i - 1];
-	}
 	else
-	{
-		printf("dest < src: Перекрытие!\n");
 		while (n > ++i)
 			dest_tmp[i] = src_tmp[i];
-	}
 	return (dest_tmp);
 }
